31301596ece01c48a96a6e17fab4317da808f9b4
Daniel Barabander <barabander@gmail.com>
2014-07-26 11:47:08 -0400

    working geolocation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,44 @@
+var map;
+
+
 function initialize() {
+  var address1 = [];
+  var address2 = [];
+  
+  geocoder = new google.maps.Geocoder();
+  var mapOptions = {
+    zoom: 12
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
 
-  var markers = [];
-  var map = new google.maps.Map(document.getElementById('map-canvas'), {
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  });
+  // Try HTML5 geolocation
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = new google.maps.LatLng(position.coords.latitude,
+                                       position.coords.longitude);
 
-  var defaultBounds = new google.maps.LatLngBounds(
-      new google.maps.LatLng(-33.8902, 151.1759),
-      new google.maps.LatLng(-33.8474, 151.2631));
-  map.fitBounds(defaultBounds);
+      // var infowindow = new google.maps.InfoWindow({
+      //   map: map,
+      //   position: pos,
+      //   content: 'Location found using HTML5.'
+      // });
+
+      map.setCenter(pos);
+    }, function() {
+      handleNoGeolocation(true);
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleNoGeolocation(false);
+  }
+
+var markers = [];
 
   // Create the search box and link it to the UI element.
-  var input1 = /** @type {HTMLInputElement} */(
-      document.getElementById('location_one'));
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input1);
-
-  var input2 = /** @type {HTMLInputElement} */(
-      document.getElementById('location_two'));
-  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input2);
+  var input = /** @type {HTMLInputElement} */(
+      document.getElementById('what_do'));
+  // map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 
   var searchBox = new google.maps.places.SearchBox(
     /** @type {HTMLInputElement} */(input));
@@ -68,6 +89,78 @@ function initialize() {
     var bounds = map.getBounds();
     searchBox.setBounds(bounds);
   });
+
+
+$('#search').click(function(){
+
+    address1Val = $("#location_one").val(); 
+    address2Val = $("#location_two").val();
+
+  
+  while (address1.length < 1) {
+    address1.push(address1Val);
+    getGeoCodeAddress(address1[0]);
+    }
+
+
+  while (address2.length < 1) {
+    address2.push(address2Val);
+    getGeoCodeAddress(address2[0]);
+    }
+
+     console.log(address1Val + 'val address1');
+     console.log(address1);
+     console.log(address1.length + 'length array address 1');
+    // console.log(address2Val);
+    // console.log(address2.length);
+
+    
+ 
+    
+  function getGeoCodeAddress(address) {
+    geocoder.geocode( { 'address': address}, function(results, status) {
+      if (status == google.maps.GeocoderStatus.OK) {
+        map.setCenter(results[0].geometry.location);
+        var marker = new google.maps.Marker({
+            map: map,
+            position: results[0].geometry.location
+        });
+      } else {
+        // alert("Geocode was not successful for the following reason: " + status);
+      }
+    });
+    }
+});
+
+
+function codeAddress() {
+  
+  }
+
+
+}
+
+function handleNoGeolocation(errorFlag) {
+  if (errorFlag) {
+    var content = 'Error: The Geolocation service failed.';
+  } else {
+    var content = 'Error: Your browser doesn\'t support geolocation.';
+  }
+
+  var options = {
+    map: map,
+    position: new google.maps.LatLng(60, 105),
+    content: content
+  };
+
+  var infowindow = new google.maps.InfoWindow(options);
+  map.setCenter(options.position);
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
+
+
+
+
+
+
